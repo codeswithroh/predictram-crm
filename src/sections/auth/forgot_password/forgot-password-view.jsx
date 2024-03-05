8570d3037607408b6be01cfb5a019c6e1217f130
ujwxl23 <ujwalkr23@gmail.com>
2024-03-05 21:17:06 +0530

    Forgot Password modified

--- src/sections/auth/forgot_password/forgot-password-view.jsx
+++ src/sections/auth/forgot_password/forgot-password-view.jsx
@@ -7,17 +7,21 @@ import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { alpha, useTheme } from '@mui/material/styles';
+import InputAdornment from '@mui/material/InputAdornment';
 
 import { bgGradient } from 'src/theme/css';
 
 import Logo from 'src/components/logo';
+import Iconify from 'src/components/iconify';
 
 export default function ForgotPasswordView() {
   const theme = useTheme();
   const { register, handleSubmit } = useForm();
   const [otpSent, setOtpSent] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const onSubmit = (data) => {
     try {
@@ -26,6 +30,8 @@ export default function ForgotPasswordView() {
         setOtpSent(true);
       } else {
         console.log('Verify OTP:', data.otp);
+        console.log('New Password:', data.newPassword);
+        console.log('Confrim Password:', data.confirmpassword);
       }
     } catch (error) {
       toast.error('Failed to reset password');
@@ -74,7 +80,43 @@ export default function ForgotPasswordView() {
                 />
               )}
 
-              {otpSent && <TextField name="otp" label="OTP" required {...register('otp')} />}
+               {otpSent && (
+                <>
+                  <TextField name="otp" label="OTP" required {...register('otp')} />
+                  <TextField
+                        name="newPassword"
+                        label="New Password"
+                        type={showPassword ? 'text' : 'password'}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                                        <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                        {...register('newPassword')}
+                        required
+                    />
+                  <TextField
+                        name="confirmPassword"
+                        label="Confirm Password"
+                        type={showPassword ? 'text' : 'password'}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                                        <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                        {...register('confirmPassword')}
+                        required
+                    />
+                </>
+              )}
             </Stack>
 
             <LoadingButton
@@ -88,36 +130,6 @@ export default function ForgotPasswordView() {
               {otpSent ? 'Submit' : 'Send OTP'}
             </LoadingButton>
           </form>
-
-          {/* <form onSubmit={handleSubmit(onSubmit)}>
-            <Stack direction={otpSent ? 'column' : 'row'} spacing={3}>
-              {!otpSent && (
-                <Stack spacing={2} alignItems="center">
-                  <TextField
-                    sx={{ width: 1 }}
-                    name="email"
-                    label="Email"
-                    type="email"
-                    required
-                    {...register('email')}
-                  />
-                </Stack>
-              )}
-
-              {otpSent && <TextField name="otp" label="OTP" required {...register('otp')} />}
-            </Stack>
-
-            <LoadingButton
-              fullWidth
-              size="large"
-              type="submit"
-              variant="contained"
-              color="inherit"
-              sx={{ padding: '10px', marginTop: '20px' }}
-            >
-              {otpSent ? 'Submit' : 'Send OTP'}
-            </LoadingButton>
-          </form> */}
         </Card>
       </Stack>
     </Box>
